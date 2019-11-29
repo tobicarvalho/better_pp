8b56e73eb184adde1294d06dc7bfd9a0910a24d6
tobicarvalho <tobi.carvalho@gmail.com>
2019-11-29 11:23:23 -0500

    first commit

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+echo "This is an empty launch script. Update it to launch your application."
+roslaunch lane_pp lane_pp.launch
